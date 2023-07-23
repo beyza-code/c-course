b0751a8ceb2635e4a31cff064546380ae2e9dd62
beyza-code <beyzalgullu@gmail.com>
2023-07-23 12:46:44 +0300

    add: for1.c

---
+++ for/for1.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+
+// Kullanıcıdan bir adet sayı girmesini isteyiniz 1'den gireceğiniz sayıya kadar
+// olan sayıların toplamını for döngüsüyle hesaplayınız.
+
+int main() {
+
+  int number;
+  int i, total = 0;
+
+  printf("Please enter a number\n");
+
+  scanf("%d", &number);
+
+  for (i = 1; i <= number; i++) {
+    total += i;
+  }
+  printf("total:%d", total);
+
+  return 0;
+}*/
