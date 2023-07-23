03ed7c5271eddaaf23d78ca47d3e4a18028444e5
beyza-code <beyzalgullu@gmail.com>
2023-07-23 12:46:56 +0300

    add: for2.c

---
+++ for/for2.c
@@ -0,0 +1,20 @@
+/*#include <stdio.h>
+
+int main() {
+
+  // 0'dan 10'a kadar olan tek ve çift sayıları for döngüsü ile ekrana
+  // bastırınız
+
+  int i;
+
+  for (i = 0; i <= 10; i += 2) {
+
+    printf("%d\n", i);
+  }
+  for (i = 1; i <= 10; i += 2) {
+
+    printf("%d\n", i);
+  }
+
+  return 0;
+}*/
