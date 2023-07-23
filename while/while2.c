b921171d92b8e6801e89ee43311a5f3eff25d405
beyza-code <beyzalgullu@gmail.com>
2023-07-23 20:41:32 +0300

    add:while2.c

---
+++ while/while2.c
@@ -0,0 +1,10 @@
+/*#include <stdio.h>
+// 10'dan başlayıp bir azalta azalta 1' e kadar olan sayıları ekrana bastırınız
+
+int main() {
+  int i = 10;
+  while (i > 0) {
+    printf("%d\n", i);
+    i--;
+  }
+}*/
