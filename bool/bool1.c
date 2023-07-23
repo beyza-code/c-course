1dd11b814749707fabc687ba5cbe863de0b50302
beyza-code <beyzalgullu@gmail.com>
2023-07-23 21:02:25 +0300

    add: bool1.c

---
+++ bool/bool1.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  bool _true = true;
+  bool _false = false;
+
+  printf("%d\n", _true);
+  printf("%d", _false);
+
+  return 0;
+}
