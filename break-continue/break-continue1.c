61661447e5ac74244f671dbd001adf5640b1c409
beyza-code <beyzalgullu@gmail.com>
2023-07-23 20:54:25 +0300

    add: break-continue1.c

---
+++ break-continue/break-continue1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+  int i;
+  for (i = 0; i < 10; i++) {
+    if (i == 5) {
+      continue;
+    }
+    if (i == 8) {
+      break;
+    }
+    printf("%d\n", i);
+  }
+  return 0;
+}
