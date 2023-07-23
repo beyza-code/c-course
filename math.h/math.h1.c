5fb735d258fc5f969f909ea1fff01feb3aa45ee5
beyza-code <beyzalgullu@gmail.com>
2023-07-23 21:57:21 +0300

    add: math.h1.c

---
+++ math.h/math.h1.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  // ceil , floor , sqrt , pow , abs
+
+  printf("%.2f\n", ceil(3.4));
+  printf("%.2f\n", floor(5.8));
+  printf("%.2f\n", sqrt(81));
+  printf("%.2f\n", pow(2,4));
+  printf("%d\n", abs(-15));
+
+  return 0;
+}
