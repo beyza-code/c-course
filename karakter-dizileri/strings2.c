063c6314b2f534eff261a14b86c16784d03cc963
beyza-code <beyzalgullu@gmail.com>
2023-07-23 22:14:40 +0300

    add: strings2.c

---
+++ karakter-dizileri/strings2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// String içerisindeki bir karaıkteri değiştirme
+
+int main() {
+
+  char dizim[] = "Beyza Algüllü";
+  dizim[0] = 'F';
+  printf("%s", dizim);
+  return 0;
+}
