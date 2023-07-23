c5a411e39a9d1b4b9cf8008c679a09f18cd2fdfa
beyza-code <beyzalgullu@gmail.com>
2023-07-23 22:15:06 +0300

    add: strlen.c

---
+++ karakter-dizileri/strlen1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Strlen Kullanımı
+
+int main() {
+  char beyza[] = "July";
+  printf("The length of the character sequence:%d\n", strlen(beyza));
+  printf("the length of the character sequence:%d\n", sizeof(beyza));
+
+  return 0;
+}
