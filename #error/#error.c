84b6cfcb9125e064593852d535f9a347149a1fb2
beyza-code <beyzalgullu@gmail.com>
2023-07-29 18:11:55 +0300

    add: #error.c

---
+++ #error/#error.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+#define NUMBER 5
+#define POWER 1
+
+#if !defined POWER || POWER < 2
+#error POWER is not defined or is less than 2
+#endif
+
+int main(){
+  return 0;
+}
